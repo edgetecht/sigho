fed2d5eea55307164443b9a32ab45c4271bbfff0
Lord Alucard Dhampir <34459507+edgetecht@users.noreply.github.com>
2022-12-05 00:56:44 -0500

    Update settings.py

--- sigho/settings.py
+++ sigho/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1csf=cez)fjp*f*_j0=rbeobbbn$+er=^j-jkzb4h)tl1v_xaw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
